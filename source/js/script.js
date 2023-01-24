61f39db331433b3d1471f457bdef39ff964047ca
Arseniy Berezin <b-ars07@yandex.ru>
2023-01-24 17:26:10 +0300

    Revert "Финал"

--- source/js/script.js
+++ source/js/script.js
@@ -1,14 +1,12 @@
-let navMain = document.querySelector('.main-header__nav');
-let navToggle = document.querySelector('.main-header__nav-toggle');
-
-navMain.classList.remove('main-header__nav--nojs');
+let navMain = document.querySelector('.main-nav');
+let navToggle = document.querySelector('.main-nav__toggle');
 
 navToggle.addEventListener('click', function () {
-  if (navMain.classList.contains('main-header__nav--closed')) {
-    navMain.classList.remove('main-header__nav--closed');
-    navMain.classList.add('main-header__nav--opened');
+  if (navMain.classList.contains('main-nav--closed')) {
+    navMain.classList.remove('main-nav--closed');
+    navMain.classList.add('main-nav--opened');
   } else {
-    navMain.classList.add('main-header__nav--closed');
-    navMain.classList.remove('main-header__nav--opened');
+    navMain.classList.add('main-nav--closed');
+    navMain.classList.remove('main-nav--opened');
   }
 });
