6a010c055d38c302104bab98562c7561fc35875c
Сергей <serega16.92@inbox.ru>
2023-01-30 14:30:07 +0500

    Защита

--- source/js/script.js
+++ source/js/script.js
@@ -3,7 +3,7 @@ let navWrapper = document.querySelector('.main-header__nav-wrapper');
 let navToggle = document.querySelector('.main-header__nav-toggle');
 
 navMain.classList.remove('main-header__nav--nojs');
-navWrapper.classList.remove('main-header__nav--nojs');
+navWrapper.classList.remove('main-header__nav-wrapper--nojs');
 
 navToggle.addEventListener('click', function () {
   if (navMain.classList.contains('main-header__nav--closed')) {
